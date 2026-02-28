44fb2601e9b67043f1e5243ed6504b28e3be4ba8
leka19lll <leka19lll@gmail.com>
2026-02-27 20:19:34 -0800

    Add files via upload

---
+++ slide1/script.js
@@ -0,0 +1,18 @@
+    <script>
+
+        var myImages = ["image1.jpg", "image2.jpg", "image3.jpg","image5.jpg","image5.jpg"];
+        var currentImage = 0;
+        document.getElementById("next").onclick = nextPhoto;
+        document.getElementById('previous').onclick = previousPhoto;
+        function nextPhoto (){
+            currentImage++;
+            if (currentImage> myImages.length -1) {currentImage = 0;}
+            document.getElementById('myimage').src = myImages[currentImage];
+        }
+        function previousPhoto (){
+            currentImage--;
+            if (currentImage<0){currentImage=myImages.length-1}
+            document.getElementById('myimage').src = myImages[currentImage];
+        }
+
+    </script>
